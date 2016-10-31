7a76396cc86a1b51f0f428e34587dc6cf5dbb4b5
Irfan Raza <irfaan.aa@gmail.com>
2016-10-31 19:21:16 +0530

    First version without animation

---
+++ app/src/main/java/com/irinnovative/onepagesigninsignup/MainActivity.java
@@ -0,0 +1,76 @@
+package com.irinnovative.onepagesigninsignup;
+
+import android.os.Bundle;
+import android.support.percent.PercentLayoutHelper;
+import android.support.percent.PercentRelativeLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvSignupInvoker;
+    private LinearLayout llSignup;
+    private TextView tvSigninInvoker;
+    private LinearLayout llSignin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvSignupInvoker = (TextView) findViewById(R.id.tvSignupInvoker);
+        tvSigninInvoker = (TextView) findViewById(R.id.tvSigninInvoker);
+
+        llSignup = (LinearLayout) findViewById(R.id.llSignup);
+        llSignin = (LinearLayout) findViewById(R.id.llSignin);
+
+        tvSignupInvoker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignupForm();
+            }
+        });
+
+        tvSigninInvoker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSigninForm();
+            }
+        });
+        showSigninForm();
+    }
+
+    private void showSignupForm() {
+        PercentRelativeLayout.LayoutParams paramsLogin = (PercentRelativeLayout.LayoutParams) llSignin.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoLogin = paramsLogin.getPercentLayoutInfo();
+        infoLogin.widthPercent = 0.10f;
+        llSignin.requestLayout();
+
+
+        PercentRelativeLayout.LayoutParams paramsSignup = (PercentRelativeLayout.LayoutParams) llSignup.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoSignup = paramsSignup.getPercentLayoutInfo();
+        infoSignup.widthPercent = 0.90f;
+        llSignup.requestLayout();
+
+        tvSignupInvoker.setVisibility(View.GONE);
+        tvSigninInvoker.setVisibility(View.VISIBLE);
+    }
+
+    private void showSigninForm() {
+        PercentRelativeLayout.LayoutParams paramsLogin = (PercentRelativeLayout.LayoutParams) llSignin.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoLogin = paramsLogin.getPercentLayoutInfo();
+        infoLogin.widthPercent = 0.90f;
+        llSignin.requestLayout();
+
+
+        PercentRelativeLayout.LayoutParams paramsSignup = (PercentRelativeLayout.LayoutParams) llSignup.getLayoutParams();
+        PercentLayoutHelper.PercentLayoutInfo infoSignup = paramsSignup.getPercentLayoutInfo();
+        infoSignup.widthPercent = 0.10f;
+        llSignup.requestLayout();
+
+        tvSignupInvoker.setVisibility(View.VISIBLE);
+        tvSigninInvoker.setVisibility(View.GONE);
+    }
+}
